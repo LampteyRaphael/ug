12449e9cb4a78ddb385df06a0d9599877edfbcb1
Raphael Lampey <raphaellampey@Raphaels-MacBook-Pro.local>
2023-12-13 15:22:17 +0000

    Ug First Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./components/Dashboard/home";
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/dashboard';
+import Grade from './components/Doc/Grade';
+import GradeForm from './components/Doc/Grade-Form';
+import Instructors from './components/Doc/Instructors';
+import Support from './components/Doc/Support';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route>
+            <Route  path="*" element={<Home />} />{/* Take this Route as a URL  example ug.edu.gh/login, ug.edu.gh/dashboard */} 
+            <Route  path="/login" element={<Login />} />
+            <Route  path="/dashboard" element={<Dashboard />} />    
+            <Route  path="/home" element={<Home />} />    
+            <Route  path="/grade" element={<Grade />} />    
+            <Route  path="/grade-form" element={<GradeForm />} />    
+            <Route  path="/instructor" element={<Instructors />} />    
+            <Route  path="/support" element={<Support />} />      
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/components/Dashboard/dashboard.js
@@ -0,0 +1,118 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+import ReactApexChart from "react-apexcharts";
+
+
+
+
+const state = {
+  series: [44, 55, 41, 17, 15],
+  options: {
+    chart: {
+      type: 'donut',
+    },
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 100,
+          height:100
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  },
+};
+
+
+const state1 = {
+        
+  series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+  options: {
+    chart: {
+      type: 'bar',
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: true,
+      }
+    },
+    dataLabels: {
+      enabled: true
+    },
+    xaxis: {
+      categories: ['Course one', 'Course Two', 'Course Three', 'Course Four', 'Course Five'
+      ],
+    }
+  },
+
+
+};
+
+const state2 = {
+          
+  series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+  options: {
+    chart: {
+      type: 'bar',
+      height: 350
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: false,
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      categories: ['Semester one 2020', 'Semester Two 2020', 'Semester One 2021', 'Semester Two 2021', 'Semester One 2022','Semester Two 2022'
+      ],
+    }
+  },
+
+
+};
+
+
+
+export default function Dashboard(){
+
+ 
+
+  return(
+    <Layout>
+
+<h1 className="mb-2 mt-0 text-5xl  font-medium leading-tight text-primary">Dashboard</h1>
+
+
+<div className="grid grid-cols-1 gap-4">
+<div className="row-span-3 col-span-1">
+    <div id="chart">
+    <ReactApexChart options={state1.options} series={state1.series} type="bar" height={350} />
+  </div>
+</div>
+<div className="row-span-3 col-span-1">
+    <div id="chart">
+      <ReactApexChart options={state.options} series={state.series} type="donut" />
+     </div>
+</div>
+</div>
+    </Layout>
+  )
+}
+
+
+
+
+
+
